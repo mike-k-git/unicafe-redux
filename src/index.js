479946052701725fd0d21a8ebefea0852fc645a3
mike-k-git <mikehsm@outlook.com>
2022-04-17 22:53:07 +0300

    6.2: unicafe revisited, step2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import reducer from './reducer'
@@ -6,21 +6,19 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
+  const updateState = (type) => {
+    store.dispatch({ type })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => updateState('GOOD')}>good</button>
+      <button onClick={() => updateState('OK')}>ok</button>
+      <button onClick={() => updateState('BAD')}>bad</button>
+      <button onClick={() => updateState('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
